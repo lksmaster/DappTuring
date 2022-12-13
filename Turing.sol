59914cd61672c14ae8d123017f0bdb7a7ed76f1e
lksmaster <47121158+lksmaster@users.noreply.github.com>
2022-12-13 17:18:50 -0300

    Add files via upload

---
+++ Turing.sol
@@ -0,0 +1,93 @@
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+ 
+contract Turing is ERC20 {
+   //address owner = msg.sender;
+   address public professora = 0xA5095296F7fF9Bdb01c22e3E0aC974C8963378ad;
+   bool[20][20] public jaVotou;
+   bool votacaoFinalizada = false;
+ 
+   modifier onlyProf() {
+       // acesso apenas da professora
+       require(msg.sender == professora);
+       _;
+   }
+   modifier podeVotar(string memory nome, uint256 amount) {
+       // condicoes para poder votar
+       require(
+           jaVotou[id[msg.sender]][id[endereco[nome]]] == false && // nao votou nesse nome ainda
+               amount <= 2 * 10**18 && // limite de 2 turings
+               msg.sender != endereco[nome] && // nao esta votando em si mesmo
+               votacaoFinalizada == false); // a votacao ainda esta aberta
+       _;
+   }
+   mapping(string => address) private endereco;
+   mapping(address => uint256) private id;
+ 
+   constructor() ERC20("Turing", "TUR") {
+       endereco["Andre"] = 0xD07318971e2C15b4f8d3d28A0AEF8F16B9D8EAb6;
+       endereco["Antonio"] = 0x127B963B9918261Ef713cB7950c4AD16d4Fe18c6;
+       endereco["Ratonilo"] = 0x5d84D451296908aFA110e6B37b64B1605658283f;
+       endereco["eduardo"] = 0x500E357176eE9D56c336e0DC090717a5B1119cC2;
+       endereco["Enzo"] = 0x5217A9963846a4fD62d35BB7d58eAB2dF9D9CBb8;
+       endereco["Fernando"] = 0xFED450e1300CEe0f69b1F01FA85140646E596567;
+       endereco["Juliana"] = 0xFec23E4c9540bfA6BBE39c4728652F2def99bc1e;
+       endereco["Altoe"] = 0x6701D0C23d51231E676698446E55F4936F5d99dF;
+       endereco["Salgado"] = 0x8321730F4D59c01f5739f1684ABa85f8262f8980;
+       endereco["Regata"] = 0x4A35eFD10c4b467508C35f8C309Ebc34ae1e129a;
+       endereco["Luis"] = 0xDD551702Dc580B7fDa2ddB7a1Ca63d29E8CDCf33;
+       endereco["Nicolas"] = 0x01fe9DdD4916019beC6268724189B2EED8C2D49a;
+       endereco["Rauta"] = 0x726150C568f3C7f1BB3C47fd1A224a5C3F706BB1;
+       endereco["Silva"] = 0xCAFE34A88dCac60a48e64107A44D3d8651448cd9;
+       endereco["Sophie"] = 0xDfb0B8b7530a6444c73bFAda4A2ee3e482dCB1E3;
+       endereco["Thiago"] = 0xBeb89bd95dD9624dEd83b12dB782EAE30805ef97;
+       endereco["Brito"] = 0xEe4768Af8caEeB042Da5205fcd66fdEBa0F3FD4f;
+       endereco["ulopesu"] = 0x89e66f9b31DAd708b4c5B78EF9097b1cf429c8ee;
+       endereco["Vinicius"] = 0x48cd1D1478eBD643dba50FB3e99030BE4F84d468;
+       endereco["Bonella"] = 0xFADAf046e6Acd9E276940C728f6B3Ac1A043054c;
+ 
+       id[0xD07318971e2C15b4f8d3d28A0AEF8F16B9D8EAb6] = 0;
+       id[0x127B963B9918261Ef713cB7950c4AD16d4Fe18c6] = 1;
+       id[0x5d84D451296908aFA110e6B37b64B1605658283f] = 2;
+       id[0x500E357176eE9D56c336e0DC090717a5B1119cC2] = 3;
+       id[0x5217A9963846a4fD62d35BB7d58eAB2dF9D9CBb8] = 4;
+       id[0xFED450e1300CEe0f69b1F01FA85140646E596567] = 5;
+       id[0xFec23E4c9540bfA6BBE39c4728652F2def99bc1e] = 6;
+       id[0x6701D0C23d51231E676698446E55F4936F5d99dF] = 7;
+       id[0x8321730F4D59c01f5739f1684ABa85f8262f8980] = 8;
+       id[0x4A35eFD10c4b467508C35f8C309Ebc34ae1e129a] = 9;
+       id[0xDD551702Dc580B7fDa2ddB7a1Ca63d29E8CDCf33] = 10;
+       id[0x01fe9DdD4916019beC6268724189B2EED8C2D49a] = 11;
+       id[0x726150C568f3C7f1BB3C47fd1A224a5C3F706BB1] = 12;
+       id[0xCAFE34A88dCac60a48e64107A44D3d8651448cd9] = 13;
+       id[0xDfb0B8b7530a6444c73bFAda4A2ee3e482dCB1E3] = 14;
+       id[0xBeb89bd95dD9624dEd83b12dB782EAE30805ef97] = 15;
+       id[0xEe4768Af8caEeB042Da5205fcd66fdEBa0F3FD4f] = 16;
+       id[0x89e66f9b31DAd708b4c5B78EF9097b1cf429c8ee] = 17;
+       id[0x48cd1D1478eBD643dba50FB3e99030BE4F84d468] = 18;
+       id[0xFADAf046e6Acd9E276940C728f6B3Ac1A043054c] = 19;
+   }
+ 
+   function issueToken(address receiver, uint256 amount) public onlyProf {
+       _mint(receiver, amount);
+   }
+ 
+   function vote(string memory nome, uint256 amount) public podeVotar(nome, amount)
+   {
+       _mint(msg.sender, 2 * 10**17); // 0,2 Turings para quem esta votando
+       _mint(endereco[nome], amount);
+       jaVotou[id[msg.sender]][id[endereco[nome]]] = true;
+   }
+ 
+   function endVoting() public onlyProf {
+       votacaoFinalizada = true; // fim da votacao
+   }
+ 
+   function balanceOfCodinome(string memory nome) public view returns (uint256){
+       return balanceOf(endereco[nome]);
+   }
+
+   function profDonate(string memory nome, uint256 amount) public {
+       issueToken(endereco[nome], amount);
+   }
+}
---
+++ index.js
@@ -0,0 +1,581 @@
+// 1. Declaração de variável global para armazenar a instância web3
+let TuringContract;
+
+// 2. Configuração do endereço do contrato e ABI
+const Turing_Contract_Address = "0x977107239aAD3bE7c7516Ba361660662e194E12B";
+const Turing_Contract_ABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "codename",
+				"type": "string"
+			}
+		],
+		"name": "balanceOfCodinome",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "codenameToAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "subtractedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "decreaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "endVoting",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "addedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "increaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "receiver",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "issueToken",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "codename",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "vote",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "voted",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+codinames = ["Andre", "Antonio", "Ratonilo", "eduardo", "Enzo", "Fernando", "Juliana", "Altoe", "Salgado", "Regata", "Luis", "Nicolas", "Rauta", "Silva", "Sophie", "Thiago", "Brito", "ulopesu", "Vinicius", "Bonella"]
+
+// Creating variables for reusable dom elements
+
+// main page
+const votacaoSection = document.querySelector(".votacao");
+const votarButton = document.querySelector("#votar");
+
+const main_para_acabar = document.querySelector("#ir-acabar");
+const main_para_doar = document.querySelector("#ir-doar");
+main_para_acabar.style.display = "none";
+main_para_doar.style.display = "none";
+const main_para_tabela = document.querySelector("#ir-tabela");
+
+// donate page
+const doacao_de_tokens = document.querySelector(".doacao-de-tokens");
+const voltar_da_doar = document.querySelector("#ir-votar");
+const doarButton = document.querySelector("#doar");
+const backBtn = document.querySelector(".back-btn");
+
+// end page
+const finalizar_section = document.querySelector(".finalizar-section");
+const encerrarButton = document.querySelector("#acabar-votacao");
+const backBtn2 = document.querySelector(".back-btn-2");
+
+// tabela
+const tabelaSection = document.querySelector(".table-section");
+const backBtn3 = document.querySelector(".back-btn-3");
+const botaoRecarregar = document.querySelector(".refresh-btn");
+
+/* Prompt user to sign in to MetaMask */
+
+const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+provider.send("eth_requestAccounts", []).then(() => {
+  provider.listAccounts().then((accounts) => {
+    const signer = provider.getSigner(accounts[0]);
+	const signer_address = signer["_address"]
+	// CONFERIR PROF
+	if (signer_address == "0xA5095296F7fF9Bdb01c22e3E0aC974C8963378ad")
+	{
+		main_para_acabar.style.display = "block";
+		main_para_doar.style.display = "block";
+		document.querySelector(".observacao").style.display = "none";
+	}
+
+    TuringContract = new ethers.Contract(
+      Turing_Contract_Address,
+      Turing_Contract_ABI,
+      signer
+    );
+  });
+});
+
+
+const ir_para_doacao = () =>
+{
+	doacao_de_tokens.style.display = "block";
+	votacaoSection.style.display = "none";
+}
+
+main_para_doar.addEventListener("click", ir_para_doacao);
+
+const ir_para_acabar = () =>
+{
+	finalizar_section.style.display = "block";
+  	votacaoSection.style.display = "none";
+}
+
+main_para_acabar.addEventListener("click", ir_para_acabar);
+
+const botao_voltar = () =>
+{
+	finalizar_section.style.display = "none";
+	doacao_de_tokens.style.display = "none";
+	tabelaSection.style.display = "none";
+	votacaoSection.style.display = "block";
+}
+
+backBtn.addEventListener("click", botao_voltar);
+backBtn2.addEventListener("click", botao_voltar);
+backBtn3.addEventListener("click", botao_voltar);
+
+// VOTACAO
+const botao_votar = () => {
+  // update button value
+  votarButton.value = "Votando...";
+
+  const codinome = document.querySelector("#codi-name");
+  const turings = document.querySelector("#qtd-turings");
+
+  // Getting values from the inputs
+  CODINOME = codinome.value;
+  TURINGS = String(parseFloat(turings.value)*10**16) + "00";
+  TURINGS_UINT256 = ethers.BigNumber.from(TURINGS)
+
+  TuringContract.vote(CODINOME, TURINGS_UINT256)
+    .then(() => {
+      // update button value
+      votarButton.value = "Votado...";
+
+      /* Reset form */
+      codinome.value = "";
+      turings.value = "";
+
+      // update button value
+      votarButton.value = "Votar";
+    })
+    .catch((err) => {
+      // If error occurs, display error message
+      votarButton.value = "Votar";
+	  if (err.code == "UNPREDICTABLE_GAS_LIMIT")
+	  {
+		alert("Error details : Já votou, esta votando em si mesmo ou o valor ultrapassou o limite de 2 Turings.");
+	  }
+	  else
+      	alert("Error details :" + err.code);
+    });
+};
+votarButton.addEventListener("click", botao_votar);
+
+// PROF DOAR TOKENS
+const botao_doar = () => {
+	// update button value
+	doarButton.value = "Doando...";
+  
+	const codinome = document.querySelector("#codi-name-doacao");
+	const turings = document.querySelector("#doacao-turings");
+  
+	// Getting values from the inputs
+	CODINOME = codinome.value;
+	TURINGS = String(parseFloat(turings.value)*10**16) + "00";
+	TURINGS_UINT256 = ethers.BigNumber.from(TURINGS)
+  
+	TuringContract.profDonate(CODINOME, TURINGS_UINT256)
+	  .then(() => {
+		// update button value
+		doarButton.value = "Doado...";
+  
+		/* Reset form */
+		codinome.value = "";
+		turings.value = "";
+  
+		// update button value
+		doarButton.value = "Doar";
+	  })
+	  .catch((err) => {
+		// If error occurs, display error message
+		doarButton.value = "Doar";
+
+		alert("Error issueToken details :" + err.code);
+	  });
+  };
+
+doarButton.addEventListener("click", botao_doar);
+
+// ENCERRAR VOTACAO
+const botao_encerrar = () => {
+	TuringContract.endVoting()
+	  .then(() => {
+		// update button value
+		doarButton.value = "Encerrando...";
+  
+		// update button value
+		doarButton.value = "Encerrar";
+	  })
+	  .catch((err) => {
+		// If error occurs, display error message
+		doarButton.value = "Encerrar";
+
+		alert("Error issueToken details :" + err.code);
+	  });
+  };
+
+encerrarButton.addEventListener("click", botao_encerrar);
+
+// TABELA DE RESULTADO
+const getTable = async () => { 
+	tabelaSection.style.display = "block";
+  	votacaoSection.style.display = "none";
+	botaoRecarregar.innerHTML = "Carregando"
+
+	for (let index = 0; index < codinames.length; index++) {
+		const element = codinames[index];
+
+		const turings = await TuringContract.balanceOfCodinome(element);
+		
+		document.querySelector("#detail-turings-".concat(index)).innerHTML  = turings / 10**18;
+	}
+	botaoRecarregar.innerHTML = "Recarregar"
+};
+
+main_para_tabela.addEventListener("click", getTable);
+botaoRecarregar.addEventListener("click", getTable);
